392e4068212e467c22c7f1dc4d420fe9033f4b5c
Ubuntu <ubuntu@ip-172-31-24-63.ap-northeast-1.compute.internal>
2015-11-28 18:13:32 +0000

    change_production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
   config.time_zone = 'Seoul'
+  before_filter :allow_iframe_requests
+  
+  def allow_iframe_requests
+  response.headers.delete('X-Frame-Options')
+  end
+
+
 end
